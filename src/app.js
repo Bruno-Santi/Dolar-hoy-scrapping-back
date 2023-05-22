f5d2e67cf71fd78fbb0480d4b3c6c3c19c20a7c4
Bruno Santimaria <brun0.23@outlook.com>
2023-05-22 18:20:25 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-app.use("/get", async (req, res) => {
+app.get("/get", async (req, res) => {
   try {
     const info = await getInfo();
     res.status(200).json(info);
