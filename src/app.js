c047684e0c58585c76a63a016573ad8cd3a1283d
Bruno Santimaria <brun0.23@outlook.com>
2023-05-22 18:24:23 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
+app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
@@ -19,7 +20,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-app.get("/get", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     const info = await getInfo();
     res.status(200).json(info);
