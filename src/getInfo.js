9512fe773388db044f129821bc0a1d94693199c8
Bruno Santimaria <brun0.23@outlook.com>
2023-05-22 20:38:24 -0300

    updated

--- src/getInfo.js
+++ src/getInfo.js
@@ -1,7 +1,10 @@
-const { chromium, selectors } = require("playwright");
+const pw = require("playwright");
 
 const getInfo = async () => {
-  const browser = await chromium.launch();
+  const browser = await pw.chromium.connectOverCDP(
+    "wss://chrome.browserless.io?token=7d906df3-14a7-4ab3-9944-824d7a6f51a4"
+  );
+
   const page = await browser.newPage();
   await page.goto("https://dolarhoy.com/");
   const dolarBlue = await page.$eval(
