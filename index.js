e9b6398a444406fd2eed370e303a227e92829c36
Bruno Santimaria <brun0.23@outlook.com>
2023-05-23 12:40:20 -0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require("./src/app");
+const app = require("./src/index");
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`listening on ${PORT}`);
